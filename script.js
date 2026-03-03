58738562aff3bd352ffa3a0d25a4ed9c91573f2b
LaCroisssant <ya.russkuy@gmail.com>
2026-03-03 19:22:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ var redIcon = L.icon({
     popupAnchor: [1, -34],});
 
 var zoo = L.marker([57.6772, 39.8998], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/zoo.webp" width="267" height = "150"><br><a href="zoo.html">Зоопарк</a>');
+    .bindPopup('<img src="images/zoo.webp" width="267" height = "150"><br><a href="pages/zoo.html">Зоопарк</a>');
 var jd = L.marker([57.6596, 39.9635], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/jd.jpg" width="200" height = "150"><br><a href="kids.html">Детская ЖД</a>');
+    .bindPopup('<img src="images/jd.jpg" width="200" height = "150"><br><a href="pages/kids.html">Детская ЖД</a>');
 var tolga = L.marker([57.6966, 39.8283], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/tolga-monastery.jpg" width="150" height = "200"><br><a href="tolga.html">Толгский монастырь</a>');
+    .bindPopup('<img src="images/tolga-monastery.jpg" width="150" height = "200"><br><a href="pages/tolga.html">Толгский монастырь</a>');
 var yakov = L.marker([57.6586, 39.95665], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/yakov.jpg" width="220" height = "150"><br><a href="yakovlevskaya.html">Яковлевская церковь</a>');
+    .bindPopup('<img src="images/yakov.jpg" width="220" height = "150"><br><a href="pages/yakovlevskaya.html">Яковлевская церковь</a>');
 var zosima = L.marker([57.63695, 39.9033], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/zosima.png" width="150" height = "150"><br><a href="zosima.html">Храм Зосимы и Савватия</a>');
+    .bindPopup('<img src="images/zosima.png" width="150" height = "150"><br><a href="pages/zosima.html">Храм Зосимы и Савватия</a>');
 var submarine = L.marker([57.638005, 39.902105], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/submarine.webp" width="230" height = "150"><br><a href="submarine.html">Подводная лодка</a>');
+    .bindPopup('<img src="images/submarine.webp" width="230" height = "150"><br><a href="pages/submarine.html">Подводная лодка</a>');
 var park = L.marker([57.6506, 39.9324], {icon: redIcon}).addTo(map)
-    .bindPopup('<img src="images/park.webp" width="200" height = "200"><br><a href="park.html">Парк Карпаты</a>');
+    .bindPopup('<img src="images/park.webp" width="200" height = "200"><br><a href="pages/park.html">Парк Карпаты</a>');
+
